b799738f58c12b645af4c8974923798cbfa53ebf
mwsimpson01-ux <mwsimpson01@gmail.com>
2026-03-06 12:29:48 -0600

    add frennergy route

---
+++ app/frennergy/page.tsx
@@ -0,0 +1,8 @@
+export default function FrennergyPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "system-ui" }}>
+      <h1>Hot Girl Frennergy</h1>
+      <p>This is the frennergy site.</p>
+    </main>
+  )
+}
